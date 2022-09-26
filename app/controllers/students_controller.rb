4cac461db4fdaa2dae83948b1d67e82c2adbc18d
Tonny Oseko <tonnyoseko0@gmail.com>
2022-09-26 23:39:48 +0300

    commit tree d9852168614505f7e10018d1da1468c438e80e83

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,8 +1,17 @@
 class StudentsController < ApplicationController
 
+  # return all students whose first or last name matches the query
   def index
     students = Student.all
+    if params[:name]
+      students = students.where("first_name LIKE ? OR last_name LIKE ?", "%#{params[:name]}%", "%#{params[:name]}%")
+    end
     render json: students
+  end
+
+  def show
+    student = Student.find(params[:id])
+    render json: student
   end
 
 end
